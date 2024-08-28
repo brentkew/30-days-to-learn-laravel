d3ad63c31b1ee65ec236dadfafa1ff4a83a2c1dd
Khalil Ahmad <brentkew@gmail.com>
2024-08-28 17:46:21 +0500

    learn about migration and creating first migration class

---
+++ database/migrations/2024_08_28_123506_create_job_listing_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_listing', function (Blueprint $table) {
+            $table->id(); // Primary key
+            $table->string('title'); // Job title
+            $table->string('salary'); // Salary range
+            $table->string('location'); // Job location
+            $table->string('experience'); // Required experience
+            $table->string('company'); // Company name
+            $table->string('type'); // Job type (Full-time, Part-time, etc.)
+            $table->date('date_posted'); // Date when the job was posted
+            $table->timestamps(); // Created and updated timestamps
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_listing');
+    }
+};
